04efac35e71c9619f12eca3600cc8ce3aaedba5c
fred-penot <frederic.penot@gmail.com>
2016-12-15 23:07:38 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+mkdir /root/.ionic
+echo '{' > /root/.ionic/ionic.config
+echo '  "ionicServeAddress": "172.23.0.2"' >> /root/.ionic/ionic.config
+echo '}' >> /root/.ionic/ionic.config
+chmod -Rf 755 /root/.ionic
